ce89fe804f67b7d4f1666dd5e7e7cd73600f8878
conglt10 <conglt1712@gmail.com>
2019-11-21 10:39:00 +0700

    api service call endgame action

--- frontends/src/App.js
+++ frontends/src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style/scss/main.scss';
 import './style/scss/style.scss';
 import { Board } from './components/board';
 import Cell from './components/Cell';
 import TileView from './components/TileView';
 import GameEndOverlay from './components/GameEndOverlay';
+import ApiService from './services/ApiService';
+//import classes from './style/scss/Home.module.scss';
+import validateEosLiquidAccount from './helpers/validateEosLiquidAccount.js';
+import Header from './components/Header';
+const { seedPrivate } = require('eosjs-ecc');
 
-export default class App extends React.Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { board: new Board() };
+    this.state = {
+      board: new Board(),
+      displayLogin: false,
+      loginError: 'none',
+      form: {
+        username: '',
+        pass: '',
+        key: '',
+        error: ''
+      },
+      isSigningIn: false,
+      isLoggedIn: false,
+      eosAddressArr: [],
+      eosPrice: '',
+      eosBalanceArr: [],
+      eosBalanceApiUrl: 'https://nodes.get-scatter.com/v1/chain/get_account',
+      eosTokenPrefixApiUrl: 'https://www.api.bloks.io/account/',
+      eosTokenSuffixApiUrl: '?type=getAccountTokens&coreSymbol=EOS',
+      eosTotalBalance: 0,
+      eosTotalTokenBalance: 0
+    };
+
     localStorage.setItem('bestscore', 0);
   }
 
@@ -20,6 +46,14 @@ export default class App extends React.Component {
   newGame() {
     this.setState({ board: new Board() });
   }
+
+  saveGame = async () => {
+    try {
+      await ApiService.endgame(this.state.board.getScore());
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   handleKeyDown(event) {
     if (this.state.board.hasWon()) {
@@ -66,10 +100,106 @@ export default class App extends React.Component {
 
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown.bind(this));
+    this.isComponentMounted = true;
   }
 
   componentWillUnmount() {
     window.removeEventListener('keydown', this.handleKeyDown.bind(this));
+    this.isComponentMounted = false;
+  }
+
+  showHideLogin = () => {
+    this.setState({ displayLogin: !this.state.displayLogin });
+  };
+
+  login = async (event) => {
+    if (!this.state.form.username || !this.state.form.pass) {
+      this.setState({ loginError: `Please provide a username / password` });
+      return;
+    }
+    event.preventDefault();
+    const { form } = this.state;
+    this.setState({ isSigningIn: true });
+    try {
+      await ApiService.register(form);
+    } catch (e) {
+      console.log(e);
+    }
+    try {
+      await ApiService.login(form);
+    } catch (e) {
+      if (e.toString().indexOf('wrong public key') !== -1) {
+        this.setState({ loginError: `Wrong password`, isSigningIn: false });
+      } else if (e.toString().indexOf('vaccount not found') !== -1) {
+        this.setState({ loginError: `Wrong password`, isSigningIn: false });
+      } else if (e.toString().indexOf('invalid nonce') !== -1) {
+        this.setState({ loginError: `Please try again`, isSigningIn: false });
+      } else if (e.toString().indexOf('vaccount already exists') !== -1) {
+        this.setState({ loginError: `Account must be a-z 1-5`, isSigningIn: false });
+      } else if (e.toString().indexOf(`required service`) !== -1) {
+        this.setState({ loginError: `DSP Error, please try again`, isSigningIn: false });
+      } else {
+        this.setState({ loginError: e.toString(), isSigningIn: false });
+      }
+      return;
+    }
+    await ApiService.fetchAccounts(form.username, this);
+
+    this.setState({
+      isSigningIn: false,
+      isLoggedIn: true,
+      loginError: validateEosLiquidAccount(this.state.form.username),
+      displayLogin: false
+    });
+  };
+
+  logout = () => {
+    localStorage.removeItem('user_account');
+    localStorage.removeItem('user_key');
+    window.location.reload();
+  };
+
+  // attemptCookieLogin = async () => {
+  //   this.setState({ isAddingAccount: true });
+  //   let account = localStorage.getItem('user_account');
+  //   let key = localStorage.getItem('user_key');
+
+  //   if (account != null && key != null) {
+  //     this.setState({ isLoggedIn: true, form: { username: account } });
+  //     await ApiService.fetchAccounts(account, this);
+  //     await this.fetchData(this.state.eosAddressArr);
+  //   }
+  //   this.setState({ isAddingAccount: false });
+  //   return;
+  // };
+
+  // fetchData = async (eosAddressArr) => {
+  //   if (this.state.eosAddressArr) await fetchAllData(eosAddressArr, this);
+  // };
+
+  handleChangeLogin(event) {
+    const { name, value } = event.target;
+    const { form } = this.state;
+    if (name === 'pass') form.key = seedPrivate(value + form.username + 'liquidportfoliosdemo134');
+
+    this.setState({
+      form: {
+        ...form,
+        [name]: value,
+        error: ''
+      },
+      loginError: 'none'
+    });
+  }
+
+  handleChangeAccount(event) {
+    // if not lower case, make lower case, only if eos account
+    if (event.target.value.toString().match(/[A-Z]/) && event.target.value.length <= 12)
+      this.setState({
+        inputAccount: event.target.value.toString().toLowerCase(),
+        addAccountErr: ''
+      });
+    else this.setState({ inputAccount: event.target.value.toString(), addAccountErr: '' });
   }
 
   render() {
@@ -86,8 +216,25 @@ export default class App extends React.Component {
     let tiles = this.state.board.tiles
       .filter((tile) => tile.value !== 0)
       .map((tile) => <TileView tile={tile} key={tile.id} />);
+
     return (
       <div>
+        <Header
+          displayLogin={this.state.displayLogin}
+          show={this.state.displayLogin}
+          showHideLogin={this.showHideLogin}
+          login={this.login.bind(this)}
+          logout={this.logout.bind(this)}
+          handleChangeLogin={this.handleChangeLogin.bind(this)}
+          error={this.state.loginError}
+          username={this.state.form.username}
+          password={this.state.form.pass}
+          isSigningIn={this.state.isSigningIn}
+          isLoggedIn={this.state.isLoggedIn}
+          loginNameForm={this.state.form}
+        />
+
+        <button onClick={this.saveGame}>Save</button>
         <div className='scores'>
           <span className='score'>scores: {this.state.board.getScore()}</span>
           <span className='best-score'>best: {localStorage.getItem('bestscore')}</span>
@@ -103,7 +250,7 @@ export default class App extends React.Component {
         >
           {cells}
           {tiles}
-          <GameEndOverlay board={this.state.board} onRestart={this.restartGame.bind(this)} />
+          <GameEndOverlay board={this.state.board} onRestart={this.saveGame.bind(this)} />
         </div>
       </div>
     );
---
+++ frontends/src/components/UI/Login.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import classes from '../../style/scss/Login.module.scss';
+import Button from './Button';
+import Input from './Input';
+
+const login = (props) => {
+  let error, errorCss, loader;
+  const showHideClassName = props.show ? classes.displayBlock : classes.displayNone;
+  const button = props.username ? 'secondary' : 'disabled';
+  if (props.error !== 'none') {
+    error = 'primaryError';
+    errorCss = classes.loginBoxError;
+  } else {
+    error = 'primary';
+    errorCss = classes.loginBoxErrorHide;
+  }
+  if (props.isSigningIn) loader = <div className={classes.loader}></div>;
+  return (
+    <div>
+      <div onClick={props.closeLogin} className={showHideClassName}></div>
+      <section className={classes.modalMain}>
+        <div className={classes.loginBoxTitle}>LOGIN / SIGN UP</div>
+        <div className={errorCss}>{props.error}</div>
+        <div className={classes.loginBox}>
+          <div className={classes.loginBoxUsernameText}>Username</div>
+          <div className={classes.inputWidth}>
+            <Input
+              required={true}
+              skin={error}
+              onChange={props.handleChangeLogin}
+              type='text'
+              placeholder='hodlsohard'
+              name='username'
+            />
+          </div>
+        </div>
+        <div className={classes.loginBox}>
+          <div className={classes.loginBoxPasswordText}>Password</div>
+          <div className={classes.inputWidth}>
+            <Input
+              required={true}
+              skin={error}
+              onChange={props.handleChangeLogin}
+              type='password'
+              placeholder=''
+              name='pass'
+            />
+          </div>
+        </div>
+        <div className={classes.loginButton}>
+          <Button text='ENTER' skin={button} onClick={props.login} />
+        </div>
+        {/* <div className={classes.xOutButton}>
+          <img onClick={(e) => props.closeLogin(e)} alt='X out Button' />
+        </div> */}
+      </section>
+      {loader}
+    </div>
+  );
+};
+
+export default login;
